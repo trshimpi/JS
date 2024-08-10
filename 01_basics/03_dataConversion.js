4d23b354b7451f7faf8af40751b8f6eb44e3ae67
Tanmay Rajendra Shimpi <31182731+trshimpi@users.noreply.github.com>
2024-08-10 18:51:05 +0000

    Added dataConversion

---
+++ 01_basics/03_dataConversion.js
@@ -0,0 +1,47 @@
+// let score = "Tanmay"
+// let score = null
+// let score = undefined
+let score = true;
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// variable  => type       => Number() conversion
+// "33"      => string     => 33
+// "33abc"   => string     =>  NaN
+//  true     => boolean    =>  1; 
+// false     => boolean    =>  0;
+// null      => object     => 0
+// undefined => undefined  => NaN
+
+// let isLoggedIn = "Tanmay"
+// let isLoggedIn = null
+let isLoggedIn = undefined
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(isLoggedIn);
+// console.log(booleanIsLoggedIn);
+
+// variable     => Boolean() conversion
+// 1            => true; 
+// 0            => false
+// ""           => false
+// "hitesh"     => true
+// null         => false
+// undefined    => false
+
+
+// let someNumber = 33
+let someNumber = undefined
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+// variable        => String() conversion type  => conversion value
+// null            => string                    => null
+// undefined       => string                    => undefined
