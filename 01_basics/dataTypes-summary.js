d4361af7e25d0f4685ca8741914fbaef4a6134cc
Tanmay Rajendra Shimpi <31182731+trshimpi@users.noreply.github.com>
2024-08-10 19:46:56 +0000

    stack and heap bifercation of datatypes note added

--- 01_basics/dataTypes-summary.js
+++ 01_basics/dataTypes-summary.js
@@ -51,4 +51,11 @@ Data types along with typeof
        Arrays  =>  object
        Function  =>  function
        Object  =>  object
+
+    datatypes are based on memory allocation
+Primitive datatypes->  call by value (string,number,boolean,null,undefined,BigInt,symbol) ->Stack memory
+
+Reference/Non primitive data types -> call by reference ( array,object, function) -> heap memory
+
+JavaScript is a dynamically typed language. This means that you don't need to specify the data type of a variable when you declare it.
 */
