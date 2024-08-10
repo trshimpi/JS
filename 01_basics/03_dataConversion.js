81711ff2a8bd451f1c263945006af638b4126321
Tanmay Rajendra Shimpi <31182731+trshimpi@users.noreply.github.com>
2024-08-10 19:03:22 +0000

    Added conversion operations in dataConversion

--- 01_basics/03_dataConversion.js
+++ 01_basics/03_dataConversion.js
@@ -45,3 +45,58 @@ console.log(typeof stringNumber);
 // variable        => String() conversion type  => conversion value
 // null            => string                    => null
 // undefined       => string                    => undefined
+
+
+
+// *********************** Operations ***********************
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "hello"
+let str2 = " hitesh"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log( (3 + 4) * 5 % 3);
+
+// console.log(+true);
+// console.log(+"");
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
+
+let x = 3;
+const y = x++;
+
+console.log(`x:${x}, y:${y}`);
+// Expected output: "x:4, y:3"
+
+let a = 3;
+const b = ++a;
+
+console.log(`a:${a}, b:${b}`);
+// Expected output: "a:4, b:4"
+
+// link to study
+// https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Increment
