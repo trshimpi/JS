f7850891bcec7a6e5d62d227548887d1f44deb08
Tanmay Rajendra Shimpi <31182731+trshimpi@users.noreply.github.com>
2024-08-11 19:36:29 +0000

    Added scope related file

---
+++ 03_basics/02_scopes.js
@@ -0,0 +1,58 @@
+//var c = 300
+let a = 300
+if (true) {
+    let a = 10
+    const b = 20
+    // console.log("INNER: ", a);
+    
+}
+
+
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+
+function one(){
+    const username = "Tanmay"
+
+    function two(){
+        const website = "youtube"
+        console.log(username);
+    }
+    // console.log(website);
+
+     two()
+
+}
+
+// one()
+
+if (true) {
+    const username = "Tanmay"
+    if (username === "Tanmay") {
+        const website = " youtube"
+        // console.log(username + website);
+    }
+    // console.log(website);
+}
+
+// console.log(username);
+
+
+// ++++++++++++++++++ interesting ++++++++++++++++++
+
+
+console.log(addone(5))  // 6
+
+function addone(num){
+    return num + 1
+}
+
+
+
+console.log(addTwo(5))  // this will throw an error as it is an expression 
+const addTwo = function(num){
+    return num + 2
+}
