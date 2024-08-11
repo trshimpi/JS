8efbef9a94986a889955fafc0e4fe3752f85dcea
Tanmay Rajendra Shimpi <31182731+trshimpi@users.noreply.github.com>
2024-08-11 11:05:06 +0000

    Added date file

---
+++ 01_basics/07_datesinjs.js
@@ -0,0 +1,34 @@
+// Dates
+
+let myDate = new Date()
+// console.log(myDate.toString());
+// console.log(myDate.toJSON());
+// console.log(myDate.toLocaleDateString());
+// console.log(myDate.toLocaleTimeString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleString());
+// console.log(typeof myDate);
+
+// let myCreatedDate = new Date(2024, 7, 11)
+// let myCreatedDate = new Date(2024, 7, 11,5,3)
+// let myCreatedDate = new Date("2024-08-11")
+let myCreatedDate = new Date("11-08-2024")
+// console.log(myCreatedDate.toLocaleString());
+
+let myTimeStamp = Date.now()
+
+// console.log(myTimeStamp);
+// console.log(myCreatedDate.getTime());
+// console.log(Math.floor(Date.now()/1000)); // get seconds
+
+let newDate = new Date()
+console.log(newDate);
+console.log(newDate.getMonth() + 1);
+console.log(newDate.getDay());
+
+// `${newDate.getDay()} and the time `
+
+console.log(newDate.toLocaleString('default', {
+    weekday: "long",
+
+}))
