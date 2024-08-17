0d441328d63f1833de5d9b8df976b190020e77c1
Tanmay Rajendra Shimpi <31182731+trshimpi@users.noreply.github.com>
2024-08-17 17:44:35 +0000

    updated currying

--- 09_advancejs/closure.js
+++ 09_advancejs/closure.js
@@ -24,3 +24,14 @@ function makeFunc() {
 
 const myFunc = makeFunc();
 myFunc();
+
+
+function add(x){
+  return function(y){
+    return function(z){
+      return x+y+z;
+    }
+  }
+}
+
+console.log(add(4)(5)(6))
