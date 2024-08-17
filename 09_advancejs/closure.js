77b6b8596aa4da1a7affbab29b1c9475646ddd02
Tanmay Rajendra Shimpi <31182731+trshimpi@users.noreply.github.com>
2024-08-17 17:42:09 +0000

    Updated closure

--- 09_advancejs/closure.js
+++ 09_advancejs/closure.js
@@ -13,3 +13,14 @@ A closure is the combination of a function bundled together (enclosed) with refe
 In other words, a closure gives you access to an outer function's scope from an inner function. In JavaScript, closures are created every 
 time a function is created, at function creation time.
 */
+
+function makeFunc() {
+  const name = "Mozilla";
+  function displayName() {
+    console.log(name);
+  }
+  return displayName;
+}
+
+const myFunc = makeFunc();
+myFunc();
